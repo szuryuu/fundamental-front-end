35efe91a71dd914c45675897025cf30d40d284a9
szuryuu <ilhamdzaky2007@gmail.com>
2026-05-31 15:00:55 +0700

    fix: update runner js

--- runner.js
+++ runner.js
@@ -1,20 +1,23 @@
 // runner.js
-const { runSub1Tests } = require('./sub1');
-const { runSub2Tests } = require('./sub2');
+const { runSub1Tests } = require("./sub1");
+const { runSub2Tests } = require("./sub2");
+const { runSub3Tests } = require("./sub3");
 
 const subType = process.argv[2];
 const targetDir = process.argv[3];
 
 if (!subType || !targetDir) {
-    console.error('❌ [ERROR] Insufficient arguments provided to runner.js');
-    process.exit(1);
+  console.error("❌ [ERROR] Insufficient arguments provided to runner.js");
+  process.exit(1);
 }
 
-if (subType === 'sub1') {
-    runSub1Tests(targetDir);
-} else if (subType === 'sub2') {
-    runSub2Tests(targetDir);
+if (subType === "sub1") {
+  runSub1Tests(targetDir);
+} else if (subType === "sub2") {
+  runSub2Tests(targetDir);
+} else if (subType === "sub3") {
+  runSub3Tests(targetDir);
 } else {
-    console.error(`❌ [ERROR] Unrecognized pipeline: ${subType}`);
-    process.exit(1);
+  console.error(`❌ [ERROR] Unrecognized pipeline: ${subType}`);
+  process.exit(1);
 }
